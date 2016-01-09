1a6bb10a89b7399e30bbc693c14ddb53894422db
Greta Kavaliauskaite <greta.kavaliauskaite@cohaesus.co.uk>
2016-01-09 17:24:25 +0000

    cahnged image siaze

---
+++ assets/app/core/js/upgrade-notification.js
@@ -0,0 +1,11 @@
+﻿$(function(){
+	/*** Add query string values ***/
+	$.each(location.search.slice(1).split('&'), function(index, item){
+		var kv = item.split('=');
+		if (kv.length === 2) {
+			var key = kv[0];
+			var value = decodeURIComponent(kv[1]);
+			$('*[data-dynamic-text="'+key+'"]').text(value);
+		}
+	});
+});
